7b0d512d7a0f7a8f27444d7ccfc4601c558661bc
Armando Beltran <armando_b@live.com>
2022-11-02 12:00:48 -0500

    Re-Created Project and Component Folders for Proper File Structure and Directories

--- src/Components/MusicTable/MusicTable.jsx
+++ src/Components/MusicTable/MusicTable.jsx
@@ -1,5 +1,3 @@
-import React, { useState } from 'react';
-
 const MusicTable = (props) => {
     return ( 
         
---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,7 @@
+const SearchBar = (props) => {
+    return ( 
+        
+     );
+}
+ 
+export default SearchBar;props
